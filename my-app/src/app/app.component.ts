11c74463e7d68de0819d9fca8d18a0538d5ee28e
Unknown <patra94@hotmail.fr>
2017-10-03 15:11:37 +0200

    Form validation côté code

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from './item';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,21 @@ import { Item } from './item';
 export class AppComponent implements OnInit {
 
   collection: Item[];
-  newItem: Item;
+  form: FormGroup;
+  nameCtrl: FormControl;
+  refCtrl: FormControl;
+  stateCtrl: FormControl;
+
+  constructor (fb: FormBuilder) {
+    this.nameCtrl = fb.control('', [Validators.required, Validators.minLength(2)]);
+    this.refCtrl = fb.control('', [Validators.required, Validators.minLength(4),]);
+    this.stateCtrl = fb.control(0); // Met la valeur d'index 0 par défaut
+    this.form = fb.group({
+      name: this.nameCtrl,
+      ref: this.refCtrl,
+      state: this.stateCtrl
+    });
+  }
 
   ngOnInit(): void {
     this.collection = [
@@ -17,16 +32,25 @@ export class AppComponent implements OnInit {
       new Item({reference: '2345', name: 'Patrick', state: 1}),
       new Item({reference: '3456', name: 'Quentin', state: 2})
     ];
-    this.resetNewItem();
   }
 
   addItem() {
-    this.collection.push(this.newItem);
-    this.resetNewItem();
+    this.collection.push({
+      name: this.form.get('name').value,
+      reference: this.form.get('ref').value, // permet d'envoyer la valeur ref même si le tableau attend un objet avec "reference"
+      state: this.form.get('state').value
+    });
+    this.reset();
   }
 
-  resetNewItem() {
-    this.newItem = new Item({reference: '', name: '', state: 0});
+  // Reset le formulaire
+  reset() {
+    this.form.reset();
+    this.stateCtrl.setValue(0);
+  }
+
+  isValid(champ: string) {
+    return this.form.get(champ).dirty && this.form.get(champ).hasError('minlength');
   }
 
 }
