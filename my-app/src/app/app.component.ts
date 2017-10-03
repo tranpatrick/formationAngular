738ae34bfbb80b021cda63dffb4a78b894d66eb8
Unknown <patra94@hotmail.fr>
2017-10-03 10:23:05 +0200

    Ajout composant card

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  collection: any;
+
+  ngOnInit(): void {
+    this.collection = [
+        {reference: '1234', name: 'Marina'},
+        {reference: '2345', name: 'Patrick'},
+        {reference: '3456', name: 'Quentin'},
+    ];
+  }
+
 }
