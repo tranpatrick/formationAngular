f28ed1813228cf8a89ce2045a02c596d14319474
Unknown <patra94@hotmail.fr>
2017-10-03 16:04:04 +0200

    Ng Bootstrap modal

---
+++ my-app/src/app/modal/modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  @Input() msg;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+
+}
