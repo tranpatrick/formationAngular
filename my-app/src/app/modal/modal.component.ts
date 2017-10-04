08349abe3c8fd99489b76d5e5da389f12b197e75
Unknown <patra94@hotmail.fr>
2017-10-04 10:49:38 +0200

    Redirect Modal with NgOnDestroy

--- my-app/src/app/modal/modal.component.ts
+++ my-app/src/app/modal/modal.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnDestroy, Input } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnDestroy {
 
   @Input() msg;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal, private router: Router) {}
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.router.navigate(['list']);
   }
 
 }
